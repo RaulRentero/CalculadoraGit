73c1062c88a2e42eb319fb344b22cade88eac368
RaulRentero <rrenchi1711@g.educaand.es>
2025-10-21 10:10:36 +0200

    Versión 2: Actualización de resta

--- Calculadora/src/com/ejemplo/calculadora/calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/calculadora.java
@@ -5,12 +5,25 @@ import java.util.Scanner;
 public class calculadora {
 	public static void main(String[] args) {
 		double a, b, total;
+		int c;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Valor de A: ");
 		a = sc.nextDouble();
 		System.out.print("Valor de B: ");
 		b = sc.nextDouble();
+		System.out.println("1)Sumar 2) Restar");
+		c = sc.nextInt();
 		
+		switch (c) {
+		case 1: 
+			System.out.println(sumar(a, b));
+			break;
+		case 2: 
+			System.out.println(restar(a, b));
+			break;
+		default:
+			System.out.println("Error");
+		}
 		total = sumar(a, b);
 		
 		System.out.println(total);
@@ -19,5 +32,10 @@ public class calculadora {
 	public static double sumar(double a, double b) {
 		
 		return a + b;
+	}
+	
+	public static double restar(double a, double b) {
+		
+		return a - b;
 	}
 }
